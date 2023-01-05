56a205cab3d545210c0146bddda41c77b3307f8e
themockingjet <jwoot001@gmail.com>
2023-01-05 14:11:12 +0800

    init

---
+++ ver2/keymaps/vial_rgb/keymap.c
@@ -0,0 +1,29 @@
+
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_ortho_5x4(
+        KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS, 
+        KC_P7,   KC_P8,   KC_P9,   KC_TAB, 
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS, 
+        KC_P1,   KC_P2,   KC_P3,   KC_DEL, 
+        KC_P0,   KC_P0,   KC_PDOT, KC_PENT)
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code_delay(KC_VOLU, 10);
+        } else {
+            tap_code_delay(KC_VOLD, 10);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            rgb_matrix_increase_hue();
+        } else {
+            rgb_matrix_decrease_hue();
+        }
+    }
+    return false;
+}
